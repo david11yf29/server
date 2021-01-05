2dd04382c38c3509abcfd14a973510aab082a549
david11yf29 <david11yf29@gmail.com>
2021-01-05 19:37:23 +0800

    updated

--- src/client/client.js
+++ src/client/client.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { combineReducers, applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import Routes from './Routes';
+import reducers from './reducers/index';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
